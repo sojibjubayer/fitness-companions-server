04e4e6138608830c84ac824ad0d3c9b9dbdc9c6f
unknown <sojibjubayer@gmail.com>
2023-11-06 23:10:39 +0600

    get service by id

--- index.js
+++ index.js
@@ -70,7 +70,13 @@ async function run() {
             res.send(result)
         })
 
-        
+         // get from mongodb for details by id
+         app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await FitnessServiceCollection.findOne(query)
+            res.send(result)
+          })
 
 
 
